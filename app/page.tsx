89bd7a4fcc4e40f1d1ccd01654a1b3a1c5767ffc
Steve Starck <steve@Pandora7.local>
2023-06-28 17:45:34 -0700

    add top margin for the pill on iPhones

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Graph from "@/components/home/graph";
 export default async function Home() {
   return (
     <>
-      <div className="mt-4 w-full">
+      <div className="mt-14 w-full">
         <h1
           className="animate-fade-up bg-gradient-to-t from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent dark:from-stone-300 dark:to-white"
           style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
