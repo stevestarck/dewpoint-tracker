f404db04acb926e747ca4092cc50dc0fb0d6744f
Steve Starck <steve@Pandora7.local>
2023-07-01 08:12:26 -0700

    recognize platform so you can adjust top margin

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,12 @@ import Balancer from "react-wrap-balancer";
 import Graph from "@/components/home/graph";
 
 export default async function Home() {
+  const platform = process.env.NEXT_PUBLIC_PLATFORM;
+  const className = (platform === "ios") ? "mt-14 w-full" : "mt-4 w-full";
+
   return (
     <>
-      <div className="mt-14 w-full">
+      <div className={className}>
         <h1
           className="animate-fade-up bg-gradient-to-t from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent dark:from-stone-300 dark:to-white"
           style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
